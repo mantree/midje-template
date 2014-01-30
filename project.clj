1874d3492c882846094e63592f99896a3db62a46
Tom Coupland <tom.coupland@nokia.com>
2014-01-30 17:52:34 +0000

    Corrected naming

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject genermidje "0.1.0-SNAPSHOT"
+(defproject midje-template "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
