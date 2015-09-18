20b1673027cd87cf3cb099fc9717b4325b96e8ff
Tom Coupland <tom.coupland@mixrad.io>
2015-09-18 15:55:28 +0100

    Upgrading midje and clojure version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/mantree/midje-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [midje "1.6.3"]])
